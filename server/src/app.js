d898ffc12bc1d44f3d07e78b3d6f696c9520b923
DeenaNash93 <deenanash93@gmail.com>
2026-03-04 22:05:01 +0200

    feat(recipes): add recipe routes

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const healthRoutes = require("./routes/healthRoutes");
 const userRoutes = require("./routes/userRoutes");
 const classRoutes = require("./routes/classRoutes");
+const recipeRoutes = require("./routes/recipeRoutes");
 const app = express();
 
 app.use(morgan("dev"));
@@ -24,6 +25,7 @@ app.use("/api", healthRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/classes", classRoutes);
+app.use("/api/recipes", recipeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
